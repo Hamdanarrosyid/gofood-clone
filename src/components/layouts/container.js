babd94b0f7b2ec82d48e94551a54db1c61e7aaf8
HamdanA <50565887+Hamdanarrosyid@users.noreply.github.com>
2021-06-09 18:12:07 +0700

    something what i need

--- src/components/layouts/container.js
+++ src/components/layouts/container.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = (props) =>{
     return(
-        <div className={'md:w-1/2 w-full bg-white  h-100'}>
+        <div className={'md:w-1/2 w-full bg-white m-auto p-5 h-screen'}>
             {props.children}
         </div>
     )
